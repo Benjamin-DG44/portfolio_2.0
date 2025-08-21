f75ac475018d2ffdf6cf06eb9f62e7c2e40f497f
Benjamin-DG44 <b.delaunay.guitton@gmail.com>
2025-08-22 01:52:52 +0200

    Mise en place des fondations du projet. - Suppression de fichiers inutiles - Passage au format TSX - Rédaction des variables CSS et des premières classes communes - Création d'un dossier "Components" qui contiendra les divers composants

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+
+    return (
+        <section>
+            <div className="center">
+                <p className="text-3xl text-gradient margin-home">Benjamin <br/>DELAUNAY-GUITTON</p>
+                <p className="text-2xl margin-home">Étudiant en BTS SIO option SLAM</p>
+                <p className="text-lg margin-home">Passionné par la programmation. J’entame ma <strong><i>seconde année en BTS SIO
+                    option SLAM</i></strong> à La Joliverie à Nantes. Ayant un attrait pour la <strong><i>programmation
+                    orientée objet</i></strong>, je souhaiterais me spécialiser dans le développement logiciel.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default App
